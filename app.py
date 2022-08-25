505fc5118fecde0471eac5bdb139ab157b84b481
Lakshit Jain <lakshitjainprof@gmail.com>
2022-08-25 15:26:02 +0530

    flask-server-real-world

--- app.py
+++ app.py
@@ -22,15 +22,15 @@ cors = CORS(
 )
 
 
-file1 = open(os.path.join(basedir,"models_anxiety.pkl"), "rb")
-anxiety = pickle.load(file1)
-file1.close()
+file = open(os.path.join(basedir, "finalized_model.sav"), "rb")
+model = pickle.load(file)
+file.close()
 
-file1 = open(os.path.join(basedir,"models_depression.pkl"), "rb")
+file1 = open(os.path.join(basedir, "models_depression.pkl"), "rb")
 depression = pickle.load(file1)
 file1.close()
 
-file1 = open(os.path.join(basedir,"models_stress.pkl"), "rb")
+file1 = open(os.path.join(basedir, "models_stress.pkl"), "rb")
 stress = pickle.load(file1)
 file1.close()
 
@@ -43,21 +43,37 @@ def home():
 
 @app.route("/predict", methods=["POST", "GET"])
 def predict():
-
     def predict(model, dataframe):
-        result = model.predict_proba(dataframe)
-        return result[0][1]
+        result = model.predict_proba(np.array(dataframe))
+        return result[0]
 
     if request.method == "POST":
         health_input = request.get_json(force=True)
-        
-        df = pd.DataFrame(health_input,index=[0])
-        df = df[['Age', 'Female', 'Male', 'BCS', 'BIT', 'Engineering', 'other', 'No',
-       'Yes', 'year 1', 'year 2', 'year 3', 'year 4', 'high', 'low', 'mid']]
+        df = pd.DataFrame(health_input, index=[0])
+        df = df[
+            [
+                "Age",
+                "Gender",
+                "CGPA",
+                "free_time",
+                "difficult_relax",
+                "social_media",
+                "look_forward",
+                "overreact",
+                "dry_mouth",
+                "trembling_suffocation",
+                "least_enthusiasm",
+                "feel_scared",
+                "games_OTT_platforms",
+                "offended",
+                "stay_negative",
+            ]
+        ]
         dataToSend = {}
-        dataToSend["depression"] = predict(depression, df)
-        dataToSend["anxiety"] = predict(anxiety, df)
-        dataToSend["stress"] = predict(stress, df)
+        prediction = predict(model, df)
+        dataToSend["healthy"] = prediction[0]
+        dataToSend["stressed"] = prediction[1]
+        dataToSend["critical"] = prediction[2]
         return dataToSend
 
 
